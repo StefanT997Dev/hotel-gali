2a84d6864fa69984cdc4637819ebb111cd9115ef
Stefan <stefantosic.dev@gmail.com>
2022-03-21 10:27:16 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import gali from './hotelgali.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={gali} className="App-logo" alt="logo" />
+        <p>
+          Sajt je u izradi!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
